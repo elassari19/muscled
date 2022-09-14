8a148096881dff0f3a5f67169c1e35462cbd7d2a
mac <mac@MacBook-Pro-de-mac.local>
2022-09-14 22:06:06 +0100

    add some calculations

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,24 +5,36 @@ const index = () => {
   const [value, setValue] = useState(0);
   const [rotate, setRotate] = useState(0);
 
-  const handelElement = (id, rot) => {
+  const handelElement = (id) => {
+    const diff = Math.abs(value - id);
     setValue(id);
-    setRotate(Math.abs(rot[0] - rotate) < 180 ? rot[0] : rot[1]);
+    setRotate(
+      diff <= 4
+        ? value < id
+          ? rotate - 45 * diff
+          : rotate + 45 * diff
+        : diff > 4
+        ? value < id
+          ? rotate - 45 * (diff - 8)
+          : rotate + 45 * (diff - 8)
+        : 0
+    );
   };
+
   return (
     <div
       className="relative h-96 w-96 mx-auto my-40 rounded-full border-red-500 p-20 bg-gray-500"
       style={{ transform: `rotate(${rotate}deg)`, transition: "1s" }}
     >
       {[
-        [0, 45, [-360, 0]],
-        [15, 75, [-45, 315]],
-        [45, 85, [-90, 270]],
-        [70, 70, [-135, 225]],
-        [80, 45, [180, -180]],
-        [70, 15, [-235, 135]],
-        [45, 5, [-280, 90]],
-        [15, 10, [-325, 45]],
+        [0, 45],
+        [15, 75],
+        [45, 85],
+        [70, 70],
+        [80, 45],
+        [70, 15],
+        [45, 5],
+        [15, 10],
       ].map((item, idx) => {
         const top = `${item[0]}%`;
         const left = `${item[1]}%`;
@@ -30,11 +42,11 @@ const index = () => {
         //   value > idx && value < idx + 4
         //     ? ((idx + 1) / 2) * -45
         //     : ((idx + 1) / 2) * 45;
-        // console.log(top, left);
+        // console.log(item[2]);
         return (
           <div
             key={idx}
-            onClick={() => handelElement(idx, item[2])}
+            onClick={() => handelElement(idx)}
             className={`bg-green-600 absolute px-4 py-2 rounded-full text-white cursor-pointer`}
             style={{
               top,
